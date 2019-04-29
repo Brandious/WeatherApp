424b6f9f299952a2bb5006a57f35ef1b88f819f7
Brandious <adnanfacic96@gmail.com>
2019-04-29 20:53:07 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require ('body-parser');
+
+const request = require('request');
+
+
+
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index',  {weather: null, error: null});
+});
+
+app.post('/', (req, res)=> {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+   
+    //console.log(city);
+    request(url, (err, response, body) => {
+        if(err)
+            res.render('index', {weather: null, error: err});
+        else 
+        {
+            let weather = JSON.parse(body);
+            if(weather.main == undefined)
+                res.render('index',{weather: null, error: 'Undefined'});
+            else 
+            {
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        }
+    });
+});
+
+app.listen(5500, () => {
+    console.log('Server is running...');
+});
